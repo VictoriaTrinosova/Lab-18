e32549cd55639050fba7587cff9e2b3bb20bab78
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-12-08 18:30:25 +0300

    Добавьте файлы проекта.

---
+++ Lab 18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strInput = Console.ReadLine();
+            char[] Arr = strInput.ToCharArray(); //разделяю строку на буквы
+            Stack<char> str = new Stack<char>();
+            bool f = true;
+
+            foreach (char s in Arr)
+            {
+                if (s == '('|| s == '{'|| s == '[')
+                {
+                    str.Push(s); //Если очередной символ — открывающая скобка, нужно поместить её на вершину стека
+                }
+                if (s == ')')
+                {
+                    if (str.Count == 0 || str.Pop() != '(')
+                    {
+                        f=false;
+                    }
+                }
+                if (s == '}')
+                {
+                    if (str.Count == 0 || str.Pop() != '{')
+                    {
+                        f = false;
+                    }
+                }
+                if (s == ']')
+                {
+                    if (str.Count == 0 || str.Pop() != '[')
+                    {
+                        f = false;
+                    }
+                }
+            }
+            if (str.Count != 0)
+                f = false;
+                Console.WriteLine(f);
+            Console.ReadKey();
+
+        }
+    }
+    }
